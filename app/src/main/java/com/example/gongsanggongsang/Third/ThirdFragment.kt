34f0e584acc526a69a1a70277292bb1916dcddc6
JiyunLee <sundaeluv98@gmail.com>
2021-03-21 11:52:32 +0900

    아예 걍 다 새로 만든 틀
    ㅠ..ㅠ

---
+++ app/src/main/java/com/example/gongsanggongsang/Third/ThirdFragment.kt
@@ -0,0 +1,28 @@
+package com.example.gongsanggongsang.Third
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.gongsanggongsang.R
+
+class ThirdFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_home_third, container, false)
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //val navHostFragment = childFragmentManager.findFragmentById(R.id.nav_host_third) as NavHostFragment? ?: return
+        //findNavController().navigate(R.id.third)
+    }
+}
