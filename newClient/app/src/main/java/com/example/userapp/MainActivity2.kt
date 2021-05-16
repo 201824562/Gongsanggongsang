1e321cea8a70fae1741de9f0f85a9217362ba38a
JiyunLee <sundaeluv98@gmail.com>
2021-05-16 14:31:25 +0900

    ㅇㅇㅇ

--- newClient/app/src/main/java/com/example/userapp/MainActivity2.kt
+++ newClient/app/src/main/java/com/example/userapp/MainActivity2.kt
@@ -12,11 +12,11 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.userapp.base.BaseActivity
+import com.example.userapp.base.BaseActivity2
 import com.example.userapp.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : BaseActivity<ActivityMainBinding, MainActivityViewModel>() {
+class MainActivity2 : BaseActivity2<ActivityMainBinding, MainActivityViewModel>() {
     companion object{
         val TOOLBAR_TITLE = "title"//dddddd
     }
@@ -31,7 +31,7 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainActivityViewModel>() 
 
     override fun initToolbar() {
         window.apply {
-            navigationBarColor = ContextCompat.getColor(this@MainActivity, R.color.white)
+            navigationBarColor = ContextCompat.getColor(this@MainActivity2, R.color.white)
         }
     }
 
